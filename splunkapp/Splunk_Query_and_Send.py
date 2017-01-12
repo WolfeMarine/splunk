c18bfc4ff31fd496195bcab986ee52ec465a1960
Christopher Wolfe <wolfemarine@gmail.com>
2017-01-12 13:04:13 -0800

    New Settings File
    Added a yam file for settings

--- splunkapp/Splunk_Query_and_Send.py
+++ splunkapp/Splunk_Query_and_Send.py
@@ -1,25 +1,22 @@
 import splunklib.results as results
 import splunklib.client as client
+import yaml
 
-HOST = "localhost"
-PORT = 8089
-USERNAME = "admin"
-PASSWORD = "expediarules"
+
+# Load settings config file
+with open('Splunk_Query_and_Send.yml', 'r') as f:
+    config = yaml.load(f)
+
 
 # Create a Service instance and log in
 service = client.connect(
-    host=HOST,
-    port=PORT,
-    username=USERNAME,
-    password=PASSWORD)
-
-# Print installed apps to the console to verify login
-#for app in service.apps:
-#    print app.name
+    host=config["Splunk_Settings"]["SplunkServer"],
+    port=config["Splunk_Settings"]["SplunkPort"],
+    username=config["Splunk_Settings"]["SplunkAccount"],
+    password=config["Splunk_Settings"]["SplunkPassword"])
 
 
 # Run an export search and display the results using the results reader.
-
 # Set the parameters for the search:
 # - Search everything in the last hour
 # - Run a normal-mode search
@@ -31,6 +28,7 @@ searchquery_export = "search * index=main | head 10"
 
 exportsearch_results = service.jobs.export(searchquery_export, **kwargs_export)
 
+
 # Get the results and display them using the ResultsReader
 reader = results.ResultsReader(exportsearch_results)
 for result in reader:
@@ -39,6 +37,3 @@ for result in reader:
     elif isinstance(result, results.Message):
         # Diagnostic messages may be returned in the results
         print "Message: %s" % result
-
-# Print whether results are a preview from a running search
-#print "is_preview = %s " % reader.is_preview
